4f3223f13fdeb405b9ecaacf54cf524eed96a555
GChanna21 <150577032+GChanna21@users.noreply.github.com>
2023-12-23 02:54:21 -0500

    Create firstscript.sh
    This is code written to check node health of vm

---
+++ firstscript.sh
@@ -0,0 +1,11 @@
+##########################
+# Author :- Channa
+# date :- 23/12/2023
+# this script outputs the node health
+# version :- V1
+#################################################################
+set -x debug mode
+df -h
+free -g
+nproc
+
